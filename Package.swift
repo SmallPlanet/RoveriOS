99f6f9d166fe4ba6d05213378c3f026b54b8f089
Rocco Bowling <rocco@chimerasw.com>
2026-01-16 23:07:51 -0500

    v0.4.35

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "RoveriOS",
-            url: "https://github.com/SmallPlanet/RoveriOS/releases/download/v0.4.34/RoveriOS.xcframework.zip",
-            checksum: "ed5665fe565473c9e643dff55399561c47e3e463f2724855427faa3d79dcae4a"
+            url: "https://github.com/SmallPlanet/RoveriOS/releases/download/v0.4.35/RoveriOS.xcframework.zip",
+            checksum: "8b65771cdf4b493e232ebb10fb7f9be2fc5aef80c6cd59fadfc641ff9a6d5886"
         )
     ]
 )
